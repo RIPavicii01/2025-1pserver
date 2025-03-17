ce01e5b263f10d360025b5dbccd27e5a4b903118
108-0 <jjang030819@gmail.com>
2025-03-17 15:37:43 +0900

    3/17(월) 오후 수업 마무리 iris 분류기 작업 마무리 및  readme 파일 작성

---
+++ front/iris.js
@@ -0,0 +1,38 @@
+function Send(){
+//    var data = {
+//        "sepal_length":8,
+//        "sepal_width":1,
+//        "petal_length":8,
+//        "petal_width":1
+//    }
+    sl = document.getElementById("sl")
+    sw = document.getElementById("sw")
+    pl = document.getElementById("pl")
+    pw = document.getElementById("pw")
+
+    var data = {
+        'sepal_length': sl.value,
+        'sepal_width': sw.value,
+        'petal_length': pl.value,
+        'petal_width': pw.value,
+    }
+
+    $.ajax(
+    {
+        type: "POST",
+    url: 'http://localhost:8000/predict',
+    headers:{
+        "Accept" : "application/json",
+        "Content-Type": "application/json",
+        },
+    data: JSON.stringify(data),
+    }).done(function(response){
+        txtOut.value = response.prediction + "  " + response.probability
+        console.log(response)
+    }).fail(function(error) {
+    alert("!/js/user.js에서 에러발생: " + error.statusText);
+    console.log(error)
+  }).always(function(r){
+    console.log("always" + r)
+  });
+}
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from irisModel import IrisMachineLearning, IrisSpecies
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 model = IrisMachineLearning()
 
